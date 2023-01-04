d309dc5f39f36e96f8095b263f8bb73971077796
muhammad-umar-divaaco05 <m.umerpervaiz@gmail.com>
2023-01-04 15:30:34 +0500

    initial commit

---
+++ scripts/after_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/ec2-user/nodejs-aws-codedeploy-pipeline 
+npm install
+# echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+# echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+# echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+# cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+# echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log 
+# npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
