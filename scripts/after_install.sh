a571e2aecf9caf7a5e4a5f8f41762385c19b7d12
muhammad-umar-divaaco05 <m.umerpervaiz@gmail.com>
2023-01-04 16:03:52 +0500

    automation

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline 
+cd /home/ec2-user/nodejs-github-codebuild-codepipeline
 npm install
 # echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 # echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
